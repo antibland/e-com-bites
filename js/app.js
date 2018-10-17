77141d72fc4b5e1d36fe1ebf1d487af80c24db1b
Andy Hoffman <antibland@gmail.com>
2018-10-17 00:54:43 -0700

    Add first example

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const bindClickBad = (path, msg) => {
+  let els = document.querySelectorAll(path);
+  els.forEach(el => {
+    el.addEventListener("click", e => {
+      /* We have to prevent default behavior, or else the hash sign appends itself
+        to the current URL. And there's that ugly page jump if we forget to prevent
+        the default behavior.
+      */
+      e.preventDefault();
+      alert(msg);
+    });
+  });
+};
+
+const bindClickGood = (path, msg) => {
+  /*
+    Another advantage to buttons: we don't need to prevent default on the event
+    object. Simpler code is more readable and less error prone.
+  */
+  document.querySelector(path).addEventListener("click", () => alert(msg));
+};
+
+export { bindClickGood, bindClickBad };
