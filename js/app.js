735d4cad610b95cdddab43988bf8ffea046fd242
Andy Hoffman <antibland@gmail.com>
2018-11-19 19:55:22 -0800

    Turn on eslint and prettify things

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const bindClickBad = (path, msg) => {
-  let els = document.querySelectorAll(path);
+  const els = document.querySelectorAll(path);
   els.forEach(el => {
-    el.addEventListener("click", e => {
+    el.addEventListener('click', e => {
       /* We have to prevent default behavior, or else the hash sign appends itself
         to the current URL. And there's that ugly page jump if we forget to prevent
         the default behavior.
@@ -17,7 +17,8 @@ const bindClickGood = (path, msg) => {
     Another advantage to buttons: we don't need to prevent default on the event
     object. Simpler code is more readable and less error prone.
   */
-  document.querySelector(path).addEventListener("click", () => alert(msg));
+  console.log('hi');
+  document.querySelector(path).addEventListener('click', () => alert(msg));
 };
 
 export { bindClickGood, bindClickBad };
